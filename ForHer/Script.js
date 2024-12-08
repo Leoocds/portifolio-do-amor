05eda86423cdfa901a6c141c5e1e72043220212c
leoocds <leoziinholimaa@gmail.com>
2024-12-08 20:36:08 -0300

    Feat: style forher project

--- ForHer/Script.js
+++ ForHer/Script.js
@@ -1,50 +1,49 @@
 const questions = [
   {
-      question: "Quando foi que nos conhecemos?",
-      options: ["14 de novembro", "04 de novembro"],
-      correctAnswer: "04 de novembro"
-    },
-    {
-      question: "O que eu mais gosto em você?",
-      options: ["Sorriso", "Cabelo", "Jeito", "Corpo"],
-      correctAnswers: ["Sorriso", "Cabelo", "Jeito", "Corpo"]
-    },
-    {
-      question: "Qual meu ídolo da mídia?",
-      options: ["Kyan", "Neymar", "Chico Moedas", "Will Smith"],
-      correctAnswer: "Kyan"
-    },
-    {
-      question: "Qual meu carro dos sonhos?",
-      options: ["Evoque", "Porsche", "Golf", "Bmw"],
-      correctAnswer: "Golf"
-    },
-    {
-      question: "Se fosse te levar pra um lugar que eu amo, onde iriamos?",
-      options: ["Qualquer praia", "Santos", "São Paulo Capital", "Outro país"],
-      correctAnswer: "Qualquer praia"
-    },
-    {
-      question: "Qual meu maior sonho?",
-      options: ["Família", "Viajar o mundo", "Casar", "Ser rico", "Ajudar o mundo"],
-      correctAnswers: ["Viajar o mundo", "Ajudar o mundo"]
-    },
-    {
-      question: "Qual foi minha maior demonstração de amor pra você?",
-      options: ["Ingresso do show do Imagine Dragons", "Todos os poemas", "Fazer uma música", "Presentes"],
-      correctAnswer: "Todos os poemas"
-    },
-    {
-      question: "Você é a razão do meu sorriso?",
-      options: ["Sim, com certeza sou", "Não, isso é muita responsabilidade..."],
-      correctAnswer: "Sim, com certeza sou"
-    },
-    {
+    question: "Quando foi que nos conhecemos?",
+    options: ["14 de novembro", "04 de novembro"],
+    correctAnswer: "04 de novembro"
+  },
+  {
+    question: "O que eu mais gosto em você?",
+    options: ["Sorriso", "Cabelo", "Jeito", "Corpo"],
+    correctAnswers: ["Sorriso", "Cabelo", "Jeito", "Corpo"]
+  },
+  {
+    question: "Qual meu ídolo da mídia?",
+    options: ["Kyan", "Neymar", "Chico Moedas", "Will Smith"],
+    correctAnswer: "Kyan"
+  },
+  {
+    question: "Qual meu carro dos sonhos?",
+    options: ["Evoque", "Porsche", "Golf", "Bmw"],
+    correctAnswer: "Golf"
+  },
+  {
+    question: "Se fosse te levar pra um lugar que eu amo, onde iriamos?",
+    options: ["Qualquer praia", "Santos", "São Paulo Capital", "Outro país"],
+    correctAnswer: "Qualquer praia"
+  },
+  {
+    question: "Qual meu maior sonho?",
+    options: ["Família", "Viajar o mundo", "Casar", "Ser rico", "Ajudar o mundo"],
+    correctAnswers: ["Viajar o mundo", "Ajudar o mundo"]
+  },
+  {
+    question: "Qual foi minha maior demonstração de amor pra você?",
+    options: ["Ingresso do show do Imagine Dragons", "Todos os poemas", "Fazer uma música", "Presentes"],
+    correctAnswer: "Todos os poemas"
+  },
+  {
+    question: "Você é a razão do meu sorriso?",
+    options: ["Sim, com certeza sou", "Não, isso é muita responsabilidade..."],
+    correctAnswer: "Sim, com certeza sou"
+  },
+  {
     question: "Você me ama?",
     options: ["Sim", "Não"],
     correctAnswer: "Sim"
-    }
-    
+  }
 ];
 
 let currentQuestionIndex = 0;
@@ -56,37 +55,32 @@ const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", checkAnswer);
 
 function showQuestion() {
-const currentQuestion = questions[currentQuestionIndex];
-questionElement.textContent = currentQuestion.question;
-optionsElement.innerHTML = "";
+  const currentQuestion = questions[currentQuestionIndex];
+  questionElement.textContent = currentQuestion.question;
+  optionsElement.innerHTML = "";
 
-currentQuestion.options.forEach(option => {
-  const button = document.createElement("button");
-  button.textContent = option;
-  button.classList.add("answer-btn");
-  button.addEventListener("click", () => selectOption(option));
-  optionsElement.appendChild(button);
-});
+  currentQuestion.options.forEach(option => {
+    const button = document.createElement("button");
+    button.textContent = option;
+    button.classList.add("answer-btn");
+    button.addEventListener("click", () => selectOption(option));
+    optionsElement.appendChild(button);
+  });
 
-if (currentQuestionIndex === questions.length - 1) {
-  const noButton = optionsElement.querySelector(`button[data-option="Não"]`);
-  noButton.classList.add("no-hover");
-}
+  if (currentQuestionIndex === questions.length - 1) {
+    const noButton = optionsElement.querySelector(`button[data-option="Não"]`);
+    noButton.classList.add("no-hover");
+  }
 
-questionElement.style.display = "block";
-optionsElement.style.display = "block";
-submitButton.disabled = true;
+  questionElement.style.display = "block";
+  optionsElement.style.display = "block";
+  submitButton.disabled = true;
 }
 
 function selectOption(option) {
   const currentQuestion = questions[currentQuestionIndex];
   currentQuestion.selectedOption = option;
   submitButton.disabled = false;
-
-  if (currentQuestionIndex === questions.length - 1) {
-    const noButton = optionsElement.querySelector(`button[data-option="Não"]`);
-    noButton.classList.add("no-hover");
-  }
 }
 
 function animateButton() {
@@ -98,55 +92,49 @@ function animateButton() {
 }
 
 function startQuiz() {
-currentQuestionIndex = 0;
-startButton.style.display = "none";
-showQuestion();
-}
-
-function selectOption(option) {
-const currentQuestion = questions[currentQuestionIndex];
-currentQuestion.selectedOption = option;
-submitButton.disabled = false;
+  currentQuestionIndex = 0;
+  startButton.style.display = "none";
+  showQuestion();
 }
 
 function checkAnswer() {
-const currentQuestion = questions[currentQuestionIndex];
-const selectedOption = currentQuestion.selectedOption;
+  const currentQuestion = questions[currentQuestionIndex];
+  const selectedOption = currentQuestion.selectedOption;
 
-if (Array.isArray(currentQuestion.correctAnswers)) {
-  if (currentQuestion.correctAnswers.includes(selectedOption)) {
-    alert("Resposta correta! 💕");
+  if (Array.isArray(currentQuestion.correctAnswers)) {
+    if (currentQuestion.correctAnswers.includes(selectedOption)) {
+      alert("Resposta correta! 💕");
+    } else {
+      alert("Resposta incorreta! 😢");
+    }
   } else {
-    alert("Resposta incorreta! 😢");
+    if (selectedOption === currentQuestion.correctAnswer) {
+      alert("Resposta correta! 💕");
+    } else {
+      alert("Resposta incorreta! 😢");
+    }
   }
-} else {
-  if (selectedOption === currentQuestion.correctAnswer) {
-    alert("Resposta correta! 💕");
+
+  currentQuestionIndex++;
+
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
   } else {
-    alert("Resposta incorreta! 😢");
+    questionElement.style.display = "none";
+    optionsElement.style.display = "none";
+
+    const finalContent = document.getElementById("finalContent");
+    finalContent.style.display = "block";
+
+    const finalMessage = document.getElementById("finalMessage");
+    finalMessage.style.display = "block";
+
+    submitButton.style.display = "none";
+
+    const finalGif = document.getElementById("finalGif");
+    finalGif.style.display = "block";
+
+    const questionarioTitulo = document.getElementById("questionarioTitulo");
+    questionarioTitulo.style.display = "none";
   }
-}
-
-currentQuestionIndex++;
-
-if (currentQuestionIndex < questions.length) {
-  showQuestion();
-} else {
-  questionElement.style.display = "none";
-  optionsElement.style.display = "none";
-
-  const finalContent = document.getElementById("finalContent");
-  finalContent.style.display = "block";
-
-  const finalMessage = document.getElementById("finalMessage");
-  finalMessage.style.display = "block";
-
-  submitButton.style.display = "none";
-
-  const finalGif = document.getElementById("finalGif");
-  finalGif.style.display = "block";
-
-  const questionarioTitulo = document.getElementById("questionarioTitulo");
-  questionarioTitulo.style.display = "none";
-}
 }
