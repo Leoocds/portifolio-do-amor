8049e35770440355c5e973987f13262ee0072d75
leoocds <leoziinholimaa@gmail.com>
2024-12-08 19:49:51 -0300

    Feat: add new project

--- Gerador do Amor/Script.js
+++ Gerador do Amor/Script.js
@@ -11,26 +11,36 @@ const compliments = [
     "Tipo Sol e Lua, separados pela distância e conectados pelo amor.",
     "I get so lost inside your eyes",
     {
-        image: "./CódigoSpotify1.png"
+        spotify: "https://open.spotify.com/embed/track/5Q7zuH8VNu7aLappSusPu6?utm_source=generator"
     },
     {
-        image: "./CódigoSpotify2.png"
+        spotify: "https://open.spotify.com/embed/track/2L2mM3rEO3arNIMQnb3dou?utm_source=generator"
     }
 ];
 
 const complimentElement = document.getElementById("compliment");
 const complimentImageElement = document.getElementById("complimentImage");
 const generateButton = document.getElementById("generateButton");
+const spotifyPlayerContainer = document.getElementById("spotifyPlayer");
 
 generateButton.addEventListener("click", () => {
     const randomCompliment = compliments[Math.floor(Math.random() * compliments.length)];
 
-    if (typeof randomCompliment === "object") {
+    if (typeof randomCompliment === "object" && randomCompliment.spotify) {
         complimentElement.textContent = "";
-        complimentImageElement.src = randomCompliment.image;
-        complimentImageElement.style.display = "block"; 
-    } else {
-        complimentElement.textContent = randomCompliment;
         complimentImageElement.style.display = "none"; 
+        spotifyPlayerContainer.innerHTML = `
+            <iframe src="${randomCompliment.spotify}" 
+                    width="300" 
+                    height="80" 
+                    frameborder="0" 
+                    allowtransparency="true" 
+                    allow="encrypted-media">
+            </iframe>`;
+        spotifyPlayerContainer.style.display = "block"; 
+    } else {
+        complimentElement.textContent = randomCompliment; 
+        complimentImageElement.style.display = "none"; 
+        spotifyPlayerContainer.style.display = "none"; 
     }
 });
